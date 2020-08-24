ff32378e3571abfb4e91511f1c5a2254218e018d
Tejaswi2737 <41263776+Tejaswi2737@users.noreply.github.com>
2020-08-24 12:58:52 +1000

    Updates

---
+++ precache-manifest.70f50ac0fa6afd33ba9defcf8954a967.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2668b38b7e451e63303297672bd7c31",
+    "url": "/COVID-19-Predictions-Victoria/index.html"
+  },
+  {
+    "revision": "caf99d0f8b1b7d4a1306",
+    "url": "/COVID-19-Predictions-Victoria/static/css/main.0ed2b26d.chunk.css"
+  },
+  {
+    "revision": "b26ab69bd1f2f648cd14",
+    "url": "/COVID-19-Predictions-Victoria/static/js/2.283e71d1.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/COVID-19-Predictions-Victoria/static/js/2.283e71d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caf99d0f8b1b7d4a1306",
+    "url": "/COVID-19-Predictions-Victoria/static/js/main.08c89f54.chunk.js"
+  },
+  {
+    "revision": "b0e5cf7f9fa72e2813d4",
+    "url": "/COVID-19-Predictions-Victoria/static/js/runtime-main.e966b319.js"
+  },
+  {
+    "revision": "8fe667c83807487f4ff55c0e7d38b2fd",
+    "url": "/COVID-19-Predictions-Victoria/static/media/Data.8fe667c8.gif"
+  },
+  {
+    "revision": "72aef60ad0515f8d385f4615f6a854a6",
+    "url": "/COVID-19-Predictions-Victoria/static/media/Math.72aef60a.PNG"
+  },
+  {
+    "revision": "45a81a2a731af3da6b2d4e77010af09f",
+    "url": "/COVID-19-Predictions-Victoria/static/media/MathGif.45a81a2a.gif"
+  },
+  {
+    "revision": "d9aac5c71b11cf78d6a74d60cf4e8b00",
+    "url": "/COVID-19-Predictions-Victoria/static/media/Stay.d9aac5c7.jpeg"
+  },
+  {
+    "revision": "b456d4bec71024f8b258eb906631b641",
+    "url": "/COVID-19-Predictions-Victoria/static/media/VicWave.b456d4be.gif"
+  }
+]);
