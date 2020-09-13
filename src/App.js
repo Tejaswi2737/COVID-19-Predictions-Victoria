d5f7302e4e80cc163ea978c6dba5e83cc1d1b857
@asd <teja37.kota@gmail.com>
2020-09-14 00:36:26 +1000

    build

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import history from "./history";
+import ReactGA from 'react-ga';
 
 
 import Dashboard from './components/Dashboard';
@@ -10,8 +11,10 @@ import Blogs from './components/Blogs';
 
 
 class App extends Component {
+  
   render() {
-
+      ReactGA.initialize('UA-177089233-1');
+      ReactGA.pageview('/#');
     return (
   
         <Router history={history} basename={process.env.PUBLIC_URL}>
