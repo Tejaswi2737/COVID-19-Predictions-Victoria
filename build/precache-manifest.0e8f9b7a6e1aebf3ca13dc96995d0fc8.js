a0c2a3bc30fcfcfb78548e11e9dbcab31faec636
@asd <teja37.kota@gmail.com>
2020-09-14 00:51:51 +1000

    codebuild

---
+++ build/precache-manifest.0e8f9b7a6e1aebf3ca13dc96995d0fc8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e041380018e6110d74ed1f87684f7f65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6e62e507e252f122e35",
+    "url": "/static/css/main.2b96f963.chunk.css"
+  },
+  {
+    "revision": "bed957166e3f22c89393",
+    "url": "/static/js/2.039124b0.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/static/js/2.039124b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6e62e507e252f122e35",
+    "url": "/static/js/main.832a6e21.chunk.js"
+  },
+  {
+    "revision": "abcb4750f6811707af55",
+    "url": "/static/js/runtime-main.346b08b4.js"
+  },
+  {
+    "revision": "8fe667c83807487f4ff55c0e7d38b2fd",
+    "url": "/static/media/Data.8fe667c8.gif"
+  },
+  {
+    "revision": "72aef60ad0515f8d385f4615f6a854a6",
+    "url": "/static/media/Math.72aef60a.PNG"
+  },
+  {
+    "revision": "45a81a2a731af3da6b2d4e77010af09f",
+    "url": "/static/media/MathGif.45a81a2a.gif"
+  },
+  {
+    "revision": "d9aac5c71b11cf78d6a74d60cf4e8b00",
+    "url": "/static/media/Stay.d9aac5c7.jpeg"
+  },
+  {
+    "revision": "b456d4bec71024f8b258eb906631b641",
+    "url": "/static/media/VicWave.b456d4be.gif"
+  }
+]);
